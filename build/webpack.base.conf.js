357c0a87c94718b880a18b5618cf7891f64f2e18
Maxim Lyatsky <mlyatsky@mindk.com>
2018-04-16 13:10:03 +0300

    feat(core): update config for work via docker. Add custom CSS file

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,10 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    app: './src/main.js',
+    styles: [
+        './src/assets/app.css',
+    ]
   },
   output: {
     path: config.build.assetsRoot,
